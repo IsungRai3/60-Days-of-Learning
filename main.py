893b209e7ff022bf18b9732ca662394570ef8161
Isung rai <raiisung9@gmail.com>
2026-06-05 18:27:48 +0545

    refactored code

--- main.py
+++ main.py
@@ -1,5 +1,72 @@
 users = []
 
+#FUNCTIONS
+def register_user():
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+
+    for user in users:
+        if user["username"] == username:
+            print("Username already exists!") 
+            return
+    else:
+        users.append({"username":username, "password":password})
+        print("User registered successfully!")
+    
+def login_user():
+
+    attempt = 3
+
+    if not users: 
+        print("No users registered")
+        return
+        
+    while attempt > 0:
+        username = input("Enter username: ")
+        password = input("Enter password: ")
+
+        for user in users:
+            if user["username"] == username and user["password"] == password:
+                print(f"Welcome {username}!")
+                return
+        attempt -= 1
+        print(f"Invalid credentials. Attempts left: {attempt}")
+
+    if attempt == 0:
+        print("Too many failed attempts! Access denied.")
+
+def user_view():
+    if users:
+        print("\n---User---")
+        for i, user in enumerate(users, start=1):
+            print(f"{i}. {user['username']}")
+    else:
+        print("No registered user!")
+    
+def user_update():
+    username = input("Enter username: ")
+
+    for user in users:
+        if user["username"] == username:
+            new_pass = input("Enter new password: ")
+            user["password"] = new_pass
+            print("User updated successfully!")
+            return
+    else:
+        print("User not found!")
+    
+def user_remove():
+    username = input("Enter username: ")
+
+    for user in users:
+        if user["username"] == username:
+            users.remove(user)
+            print("User deleted successfully!")
+            return
+    else:
+        print("User not found!")
+
+
 while True:
     print("\n=== MENU ===")
     print("1.Register user")
@@ -12,94 +79,23 @@ while True:
     choice = input("Enter your choice: ")
 
     if choice == "1":
-        username = input("Enter username: ")
-        password = input("Enter password: ")
-
-        exists = False
-
-        for user in users:
-            if user["username"] == username:
-                exists = True
-                break
-
-        if exists:
-            print("Username already exists!") 
-    
-        else:
-            users.append({"username":username, "password":password})
-            print("User registered successfully!")
-    
-    elif choice == "2":
-
-        attempt = 3
-
-        if not users: 
-                print("No users registered")
-                continue
+        register_user()
         
-        while attempt > 0:
-            username = input("Enter username: ")
-            password = input("Enter password: ")
-            found = False
-
-            for user in users:
-                if user["username"] == username and user["password"] == password:
-                    print(f"Welcome {username}!")
-                    found = True
-                    break
-                
-            if found:
-                break
-
-            if not found:
-                attempt -= 1
-                print(f"Invalid credentials. Attempts left: {attempt}")
-
-        if attempt == 0:
-            print("Too many failed attempts! Access denied.")
+    elif choice == "2":
+        login_user()
 
     elif choice == "3":
-        if users:
-            print("\n*** User ***")
-            for i, user in enumerate(users, start=1):
-                print(f"{i}. {user['username']}")
-        else:
-            print("No registered user!")
-    
+        user_view()
+
     elif choice == "4":
-        username = input("Enter username: ")
+        user_update()
 
-        found = False
-
-        for user in users:
-            if user["username"] == username:
-                new_pass = input("Enter new password: ")
-                user["password"] = new_pass
-                print("User update successfully!")
-                found = True
-                break
-        if not found:
-            print("User not found!")
-    
     elif choice == "5":
-        username = input("Enter username: ")
-        found = False
-
-        for user in users:
-            if user["username"] == username:
-                users.remove(user)
-                print("User deleted successfully!")
-                found = True
-                break
-            
-        if not found:
-            print("User not found!")
-
+        user_remove()
 
     elif choice == "6":
         print("Exiting program...")
         break
-
     else:
         print("Error: Invalid choice! Try again!")
         
